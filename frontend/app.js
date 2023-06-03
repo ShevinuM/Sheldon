2aa9749c5bc607ec86ff9e2913316ac01e96dc72
ShevinuM <nsmcooray@mun.ca>
2023-06-03 18:36:06 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -29,13 +29,47 @@ class Chatbox {
             return;
         }
 
-        let message = {name: "user", message: text}
-        this.messages.push(message)
+        let msg = {name: "user", message: text}
+        this.messages.push(msg)
 
         fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
-            body: JSON.stringify({message: text1 }),
+            body: JSON.stringify({message: text }), //convert the object to JSON
+            headers: {
+                'Content-Type': 'application/json' // indicate the server that the data in the request body is formatted in json
+            }
         })
+        .then(response => response.json()) // extract the JSON data from a response object and parse it into a JavaScript object.
+        .then(response => {
+            let reply = {name: "Sheldon", message: response.answer };
+            this.messages.push(reply)
+            this.updateChatText(chatbox)
+            textField.value = ''
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatbox)
+            textField.value = ''
+        });
+    }
 
+    updateChatText(chatbox) {
+        let html = ''
+        this.messages.slice().reverse().forEach(function(item, index) {
+            // slice() is used to make a shallow copy of the messages array to make sure that the original array is not modified
+            // reverse() is used to reverse the order of the elements in the array so that the latest message is displayed at the bottom
+            // forEach() is used to iterate over the reversed array and execute the callback function for each element
+            if (item.name == "Sam") {
+                html += '<div class="message__item visitor">' + item.message + '</div>'
+            } else {
+                html += '<div class="message__item operator">' + item.message + '</div>'
+            }
+        });
+        
+        // select the first 'chatbox_messages' element within the 'chatBox' element
+        const chatMessage = chatbox.querySelector('.chatbox__messages');
+        chatMessage.innerHTML = html;
     }
 }
+
+const chatbox = new Chatbox();
+chatbox.display();
