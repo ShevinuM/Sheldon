25c646275eb99de6b95d131bcbc2014ba2cc16b8
ShevinuM <nsmcooray@mun.ca>
2023-06-10 01:57:32 +0530

    functionality to handle submit button

--- static/form.js
+++ static/form.js
@@ -1,6 +1,9 @@
 class Form{
 
     constructor(){
+        this.form = document.querySelector('form');
+        this.submitButton = this.form.querySelector('button[type="submit"]');
+
         this.educationContainer = document.getElementById('educationContainer');
         this.educationTemplate = document.getElementById('educationTemplate');
         this.addEducationButton = document.getElementById('addEducationButton');
@@ -16,6 +19,7 @@ class Form{
         this.addEducationButton.addEventListener('click', ()=> this.addEducation().bind(this));
         this.addProjectButton.addEventListener('click', ()=> this.addProject().bind(this));
         this.addExperienceButton.addEventListener('click', ()=> this.addExperience().bind(this));
+        this.submitButton.addEventListener('click', this.onSubmit.bind(this));
     }
 
     addProject() {
@@ -31,6 +35,38 @@ class Form{
     addExperience() {
         const clone = this.experienceTemplate.content.cloneNode(true);
         this.experienceContainer.appendChild(clone);
+    }
+
+    onSubmit(event) {
+        event.preventDefault(); // prevent the default form submission behavior
+
+        const formData = new FormData(this.form);
+        const jsonData = this.converyFormDataToJson(formData);
+
+        const projects = jsonData.resume[0].projects;
+        const experience = jsonData.resume[0].experience;
+
+        const updatedProjectDescription = projects.map((project) => {
+            const generatedDescription = this.generateOpenAIDescription(project.description);
+            return {...project, description: generatedDescription};
+        });
+
+        const updatedExperienceDescription = experience.map((experience) => {
+            const generatedDescription = this.generateOpenAIDescription(experience.description);
+            return {...experience, description: generatedDescription};
+        });
+
+        const modifiedJsonData = {...jsonData };
+        modifiedJsonData.resume[0].projects = updatedProjectDescription;
+        modifiedJsonData.resume[0].experience = updatedExperienceDescription;
+
+        // TODO: Send the modifiedJsonData to the server
+        
+    }
+
+    converyFormDataToJson(formData) {
+
+        
     }
 
 }
